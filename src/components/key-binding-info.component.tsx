4729afb2eae367b53c3feb780b76eb0132189502
Ali <muhammadaliqorbanzadeh@gmail.com>
2023-05-04 02:01:58 +0300

    Fix element name

--- src/components/key-binding-info.component.tsx
+++ src/components/key-binding-info.component.tsx
@@ -6,10 +6,10 @@ function KeyBindingInfo({
   action: string;
 }) {
   return (
-    <div className="flex items-center justify-between gap-2 text-neutral-950">
+    <span className="flex items-center justify-between gap-2 text-neutral-950">
       <span className="uppercase">&lt;{keyCode}&gt;:</span>
       <span className="capitalize">{action}</span>
-    </div>
+    </span>
   );
 }
 
